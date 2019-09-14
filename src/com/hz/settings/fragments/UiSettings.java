cafbbc782de3119ab06c0dde3376c5c6d3f70d65
xyyx <xyyx@mail.ru>
2019-09-14 10:01:49 +0800

    Move to androidx

--- src/com/hz/settings/fragments/UiSettings.java
+++ src/com/hz/settings/fragments/UiSettings.java
@@ -3,9 +3,9 @@ package com.hz.settings.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v14.preference.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
